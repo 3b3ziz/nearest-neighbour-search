cf20c8f26af7ae256794aab17246c3731482bc87
3b3ziz <a.3b3ziz@gmail.com>
2020-02-19 00:24:10 +0200

    refactor: use HOF ot store nearest station

--- nn_search.py
+++ nn_search.py
@@ -1,25 +1,34 @@
 from utils import calc_distance
 
 
-def nn_search(point, tree, depth: int = 0, current_best=10 ** 10, best_values=[]):
-    if not tree:
-        return None
+def nn_search(tree, point, depth: int = 0):
 
-    k = len(tree.location)
-    axis = depth % k
+    current_best = 10 ** 10
+    nearest_station = None
 
-    # compare distance between my search point and the node location
-    # store current_best values
-    distance = calc_distance(point[1], tree.location[1])
-    print(point[1], tree.location[1])
-    if distance < current_best:
-        current_best = distance
-        best_values.append((tree.location[0], current_best))
+    def nn_search_helper(point, tree, depth: int = 0):
+        nonlocal current_best
+        nonlocal nearest_station
 
-    if point[1][axis] < tree.location[1][axis]:
-        nn_search(point, tree.left_child, depth + 1, current_best, best_values)
-    else:
-        nn_search(point, tree.right_child, depth +
-                  1, current_best, best_values)
+        if not tree:
+            return None
 
-    return best_values
+        k = len(tree.location)
+        axis = depth % k
+
+        # compare distance between my search point and the node location
+        # store current_best values
+        distance = calc_distance(point[1], tree.location[1])
+
+        if distance < current_best:
+            current_best = distance
+            nearest_station = tree.location
+
+        if point[1][axis] < tree.location[1][axis]:
+            nn_search_helper(point, tree.left_child, depth + 1)
+        else:
+            nn_search_helper(point, tree.right_child, depth + 1)
+
+    nn_search_helper(point, tree, depth)
+
+    return nearest_station[0], current_best, 
